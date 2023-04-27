5f7d5d69b4cbfc2ab2d6ed05587e0c42771e9156
Harsh Mohan <harshmohan813@gmail.com>
2023-04-27 20:38:57 +0100

    nvim: add hls in null-ls

--- nvim/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -1,5 +1,5 @@
 local status, null_ls = pcall(require, "null-ls")
-if (not status) then
+if not status then
   print("null-ls not found")
   return
 end
@@ -9,23 +9,33 @@ local lint = null_ls.builtins.diagnostics
 
 local sources = {
   -- Web development
-  formatting.prettier.with {
+  formatting.prettier.with({
     filetypes = { "html", "json", "scss", "css", "javascript", "javascriptreact", "typescript" },
-  },
+  }),
 
   -- Lua
   formatting.stylua,
-  lint.luacheck.with { extra_args = { "--global vim" } },
+  lint.luacheck.with({ extra_args = { "--global vim" } }),
 
   -- Shell
   formatting.shfmt,
-  lint.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+  lint.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 
   -- c/c++/java
-  formatting.clang_format.with { filetypes = { "java", "c", "cpp" } },
+  formatting.clang_format.with({
+    filetypes = {
+      "java",
+      "c",
+      "cpp",
+      "proto",
+    }
+  }),
+
+  -- haskell
+  formatting.stylish_haskell,
 }
 
-null_ls.setup {
+null_ls.setup({
   debug = true,
   sources = sources,
-}
+})
