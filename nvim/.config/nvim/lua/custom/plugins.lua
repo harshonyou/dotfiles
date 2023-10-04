755d8fa62cbbdf2c5d5e3743f43cc580acaca586
Harsh Mohan <harshmohan813@gmail.com>
2023-10-05 00:30:35 +0100

    nvim: add python in tree-sitter and formatting

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -1,162 +1,164 @@
 local plugins = {
-  {
-    "nvim-treesitter/nvim-treesitter",
+	{
+		"nvim-treesitter/nvim-treesitter",
 
-    opts = {
-      ensure_installed = {
-        -- defaults
-        "vim",
-        "lua",
+		opts = {
+			ensure_installed = {
+				-- defaults
+				"vim",
+				"lua",
 
-        -- low level
-        "cpp",
+				-- low level
+				"cpp",
 
-        "markdown",
-      },
-    },
-  },
+				"markdown",
 
-  {
-    "neovim/nvim-lspconfig",
+				"python",
+			},
+		},
+	},
 
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
+	{
+		"neovim/nvim-lspconfig",
 
-      config = function()
-        require("custom.configs.null-ls")
-      end,
-    },
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
 
-    config = function()
-      require("plugins.configs.lspconfig")
-      require("custom.configs.lspconfig")
-    end,
-  },
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
 
-  {
-    "williamboman/mason.nvim",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
 
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "clangd",
-        "clang-format",
-        "prettier",
-        "stylua",
-        "shfmt",
-        "luacheck",
-        "haskell-language-server",
-        "rustfmt",
-        "rust-analyzer",
-        "gopls",
-        "gofumpt",
-        "goimports",
-        "goimports-reviser",
+	{
+		"williamboman/mason.nvim",
 
-        --python
-        "pylyzer",
-        "mypy",
-        "ruff",
-        "black",
-      },
-    },
-  },
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"clangd",
+				"clang-format",
+				"prettier",
+				"stylua",
+				"shfmt",
+				"luacheck",
+				"haskell-language-server",
+				"rustfmt",
+				"rust-analyzer",
+				"gopls",
+				"gofumpt",
+				"goimports",
+				"goimports-reviser",
 
-  {
-    "lukas-reineke/indent-blankline.nvim",
+				--python
+				"pylyzer",
+				"mypy",
+				"ruff",
+				"black",
+			},
+		},
+	},
 
-    config = function()
-      vim.opt.termguicolors = true
-      vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
-      vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+	{
+		"lukas-reineke/indent-blankline.nvim",
 
-      vim.opt.list = true
-      vim.opt.listchars:append("space:⋅")
-      vim.opt.listchars:append("eol:↴")
+		config = function()
+			vim.opt.termguicolors = true
+			vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
+			vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
+			vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
+			vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
+			vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
+			vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
 
-      require("indent_blankline").setup({
-        -- show_end_of_line = true,
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
-        -- show_trailing_blankline_indent = false,
-        -- char_highlight_list = {
-        --   "IndentBlanklineIndent1",
-        --   "IndentBlanklineIndent2",
-        --   "IndentBlanklineIndent3",
-        --   "IndentBlanklineIndent4",
-        --   "IndentBlanklineIndent5",
-        --   "IndentBlanklineIndent6",
-        -- },
-      })
-    end,
-  },
+			vim.opt.list = true
+			vim.opt.listchars:append("space:⋅")
+			vim.opt.listchars:append("eol:↴")
 
-  {
-    "CRAG666/code_runner.nvim",
+			require("indent_blankline").setup({
+				-- show_end_of_line = true,
+				space_char_blankline = " ",
+				show_current_context = true,
+				show_current_context_start = true,
+				-- show_trailing_blankline_indent = false,
+				-- char_highlight_list = {
+				--   "IndentBlanklineIndent1",
+				--   "IndentBlanklineIndent2",
+				--   "IndentBlanklineIndent3",
+				--   "IndentBlanklineIndent4",
+				--   "IndentBlanklineIndent5",
+				--   "IndentBlanklineIndent6",
+				-- },
+			})
+		end,
+	},
 
-    config = function()
-      require("code_runner").setup({
-        focus = false,
-        filetype = {
-          -- java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
-          -- python = "python3 -u",
-          -- typescript = "deno run",
-          -- rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
-          dart = "dart $dir/$fileName",
-          excluded_buftypes = { "message" },
-        },
-      })
-    end,
-  },
+	{
+		"CRAG666/code_runner.nvim",
 
-  {
-    "gelguy/wilder.nvim",
+		config = function()
+			require("code_runner").setup({
+				focus = false,
+				filetype = {
+					-- java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
+					-- python = "python3 -u",
+					-- typescript = "deno run",
+					-- rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
+					dart = "dart $dir/$fileName",
+					excluded_buftypes = { "message" },
+				},
+			})
+		end,
+	},
 
-    keys = { ":", { ";", ":", mode = "n", desc = "command mode" } },
+	{
+		"gelguy/wilder.nvim",
 
-    config = function()
-      local wilder = require("wilder")
+		keys = { ":", { ";", ":", mode = "n", desc = "command mode" } },
 
-      wilder.setup({
-        next_key = "<Down>",
-        accept_key = "<Right>",
-        reject_key = "<Left>",
-        previous_key = "<Up>",
-        modes = { ":", "/", "?" },
-      })
+		config = function()
+			local wilder = require("wilder")
 
-      wilder.set_option(
-        "renderer",
+			wilder.setup({
+				next_key = "<Down>",
+				accept_key = "<Right>",
+				reject_key = "<Left>",
+				previous_key = "<Up>",
+				modes = { ":", "/", "?" },
+			})
 
-        wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-          pumblend = 20,
-          highlighter = wilder.basic_highlighter(),
-          highlights = {
-            border = "Normal", -- highlight to use for the border
-          },
-          border = "rounded",
-        }))
-      )
-    end,
-  },
+			wilder.set_option(
+				"renderer",
 
-  {
-    "iamcco/markdown-preview.nvim",
-    ft = "markdown",
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
+				wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+					pumblend = 20,
+					highlighter = wilder.basic_highlighter(),
+					highlights = {
+						border = "Normal", -- highlight to use for the border
+					},
+					border = "rounded",
+				}))
+			)
+		end,
+	},
 
-  {
-    "vimwiki/vimwiki",
-    ft = { "markdown", "vimwiki" },
-  },
+	{
+		"iamcco/markdown-preview.nvim",
+		ft = "markdown",
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+
+	{
+		"vimwiki/vimwiki",
+		ft = { "markdown", "vimwiki" },
+	},
 }
 
 return plugins
