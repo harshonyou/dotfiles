fe243d250d2c8c3bc483353f2815ab7c451f7fb5
Harsh Mohan <harshmohan813@gmail.com>
2026-05-10 13:45:28 +0100

    feat(nvim): enable global spell checking with camelCase support

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -66,3 +66,8 @@ vim.opt.hlsearch = true
 
 -- gets rid of line with white spaces
 vim.g.editorconfig = true
+
+vim.opt.spell = true
+vim.opt.spelllang = { "en_us" }
+vim.opt.spelloptions = { "camel" }
+vim.opt.spellcapcheck = ""
