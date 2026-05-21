fbf18b360ae163b0cba5dc4a51362c543ad8045b
Harsh Mohan <harshmohan813@gmail.com>
2026-05-21 13:50:19 +0100

    fix(nvim): use flash.nvim for treesitter incremental selection
    Replace manual node-stack implementation with flash.nvim's built-in
    treesitter selection. <CR> to expand, <BS> to shrink.

--- home/dot_config/nvim/lua/plugins/flash.lua
+++ home/dot_config/nvim/lua/plugins/flash.lua
@@ -10,5 +10,10 @@ return {
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<CR>", mode = { "n", "x", "o" }, function()
+      require("flash").treesitter({
+        actions = { ["<CR>"] = "next", ["<BS>"] = "prev" }
+      })
+    end, desc = "Treesitter incremental selection" },
   },
 }
