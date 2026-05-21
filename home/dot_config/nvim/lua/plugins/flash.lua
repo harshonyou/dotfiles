c51baa4f1d268e7b875e206e5abf44d273a273fa
Harsh Mohan <harshmohan813@gmail.com>
2026-05-21 14:10:30 +0100

    feat(nvim): enhance flash.nvim with jump labels and * / # motions
    Enable jump labels for f/t/F/T char motions. Add flash-powered * and #
    that label all word matches and set search register so n/N work after.

--- home/dot_config/nvim/lua/plugins/flash.lua
+++ home/dot_config/nvim/lua/plugins/flash.lua
@@ -2,7 +2,13 @@ return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	---@type Flash.Config
-	opts = {},
+	opts = {
+		modes = {
+			char = {
+				jump_labels = true,
+			},
+		},
+	},
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
@@ -10,6 +16,8 @@ return {
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "*", mode = { "n" }, function() require("flash").jump({ pattern = vim.fn.expand("<cword>"), jump = { history = true, register = true } }) end, desc = "Flash *" },
+    { "#", mode = { "n" }, function() require("flash").jump({ pattern = vim.fn.expand("<cword>"), search = { forward = false }, jump = { history = true, register = true } }) end, desc = "Flash #" },
     { "<CR>", mode = { "n", "x", "o" }, function()
       require("flash").treesitter({
         actions = { ["<CR>"] = "next", ["<BS>"] = "prev" }
